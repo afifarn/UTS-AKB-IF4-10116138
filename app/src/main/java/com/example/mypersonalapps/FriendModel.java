f4d52729d40aab03870e1dc93a1a26b3c221505e
afif raditya nugraha <afifr0837@gmail.com>
2019-05-22 00:51:01 +0700

    first commit

---
+++ app/src/main/java/com/example/mypersonalapps/FriendModel.java
@@ -0,0 +1,52 @@
+package com.example.mypersonalapps;
+/*Nama : Afif Raditya Nugraha
+NIM  : 10116138
+KELAS : AKB-IF4
+Tanggal Pengerjaan : 18 Mei 2019*/
+
+public class FriendModel {
+
+    private int nim;
+    private String nama;
+    private String kelas;
+    private String nohp;
+    private String email;
+    private String sosmed;
+
+    public int getNim(){
+        return nim;
+    }
+    public void setNim(int nim){
+        this.nim=nim;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public void setNama(String nama){
+        this.nama=nama;
+    }
+    public String getKelas(){
+        return kelas;
+    }
+    public void setKelas(String kelas){
+        this.kelas=kelas;
+    }
+    public String getNohp(){
+        return nohp;
+    }
+    public void setNohp(String nohp){
+        this.nohp=nohp;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getSosmed(){
+        return sosmed;
+    }
+    public void setSosmed(String sosmed){
+        this.sosmed=sosmed;
+    }
+}
